3573a6a1a62778f86c9196c7748fcce626c16b99
Eliana Correia <eliana.correia@outlook.com.br>
2022-09-27 16:40:43 -0300

    Projeto algalog-api

---
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -0,0 +1,54 @@
+package com.algaworks.algalog.domain.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CatalogoClienteService {
+
+	private ClienteRepository clienteRepository;
+
+	public Cliente buscar(Long clienteId) {
+		return clienteRepository.findById(clienteId).orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+	}
+
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail()).stream()
+				.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+
+		if (emailEmUso) {
+			throw new NegocioException("Já existe um cliente cadastrado com este e-mail.");
+
+		}
+
+		return clienteRepository.save(cliente);
+	}
+
+	@Transactional
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+
+	public Cliente atualizar(Cliente cliente, Long clientId) {
+
+		Optional<Cliente> clientesComEsseEmail = clienteRepository.findByEmail(cliente.getEmail());
+
+		if (clientesComEsseEmail.isPresent() && clientesComEsseEmail.get().getId() != clientId) {
+			throw new NegocioException("Email vinculado a outro cliente");
+		}
+
+		cliente.setId(clientId);
+		return clienteRepository.save(cliente);
+	}
+}
